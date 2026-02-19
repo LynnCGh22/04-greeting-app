9914c4846e0f8643daafd76c717ba7d69a07154c
Lynn Chaker <lchaker921@tamu.edu>
2026-02-19 03:09:07 +0000

    Button added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let userName = "Lynn";
 console.log("Welcome to the JavaScript world", userName + "!");
 
+// Get elements
+const button = document.getElementById("welcomeBtn");
+const message = document.getElementById("message");
+
+// Add click event
+button.addEventListener("click", function() {
+  message.textContent = `Welcome, ${userName}!`;
+});
